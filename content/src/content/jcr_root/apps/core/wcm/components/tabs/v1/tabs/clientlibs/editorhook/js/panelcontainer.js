67236eeee1760461c1c3ef1ae7ed6b909d0aae16
piyushjindal <piyushjindal@adobe.com>
2024-07-06 22:24:44 +0530

    GRANITE-53021 Localization Changes wrt to i18

--- content/src/content/jcr_root/apps/core/wcm/components/tabs/v1/tabs/clientlibs/editorhook/js/panelcontainer.js
+++ content/src/content/jcr_root/apps/core/wcm/components/tabs/v1/tabs/clientlibs/editorhook/js/panelcontainer.js
@@ -20,11 +20,11 @@
         if (window.CQ && window.CQ.CoreComponents && window.CQ.CoreComponents.panelcontainer &&
             window.CQ.CoreComponents.panelcontainer.v1 && window.CQ.CoreComponents.panelcontainer.v1.registry) {
             window.CQ.CoreComponents.panelcontainer.v1.registry.register({
-                name: "cmp-tabs",
-                selector: ".cmp-tabs",
-                wrapperSelector: '[data-panelcontainer="tabs"]',
-                itemSelector: "[data-cmp-hook-tabs='tabpanel']",
-                itemActiveSelector: ".cmp-tabs__tabpanel--active"
+                name: Granite.I18n.get("cmp-tabs"),
+                selector: Granite.I18n.get(".cmp-tabs"),
+                wrapperSelector: Granite.I18n.get('[data-panelcontainer="tabs"]'),
+                itemSelector: Granite.I18n.get("[data-cmp-hook-tabs='tabpanel']"),
+                itemActiveSelector: Granite.I18n.get(".cmp-tabs__tabpanel--active")
             });
         }
     });
