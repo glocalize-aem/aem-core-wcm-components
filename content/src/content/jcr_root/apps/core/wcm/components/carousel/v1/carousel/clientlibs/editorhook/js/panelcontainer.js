5a5e48aa5a53bd66d843ec1ed066a97909b34d85
piyushjindal <piyushjindal@adobe.com>
2024-07-07 12:05:08 +0530

    GRANITE-53021 Localization Changes wrt to i18

--- content/src/content/jcr_root/apps/core/wcm/components/carousel/v1/carousel/clientlibs/editorhook/js/panelcontainer.js
+++ content/src/content/jcr_root/apps/core/wcm/components/carousel/v1/carousel/clientlibs/editorhook/js/panelcontainer.js
@@ -31,3 +31,22 @@
     });
 
 })(jQuery(document));
+
+(function(channel) {
+    "use strict";
+
+    channel.on("cq-editor-loaded", function(event) {
+        if (window.CQ && window.CQ.CoreComponents && window.CQ.CoreComponents.panelcontainer &&
+            window.CQ.CoreComponents.panelcontainer.v1 && window.CQ.CoreComponents.panelcontainer.v1.registry) {
+            window.CQ.CoreComponents.panelcontainer.v1.registry.register({
+                name: Granite.I18n.getVar("cmp-carousel"),
+                selector: ".cmp-carousel",
+                wrapperSelector: '[data-panelcontainer="carousel"]',
+                itemSelector: Granite.I18n.getVar("[data-cmp-hook-carousel='item']"),
+                itemActiveSelector: Granite.I18n.getVar(".cmp-carousel__item--active"),
+                itemSelectorWrapper: Granite.I18n.getVar(".cmp-carousel__content")
+            });
+        }
+    });
+
+})(jQuery(document));
